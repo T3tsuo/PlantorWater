9c516beae3c8dfd2d49b0a5d60ce2f2a22cae6d0
Takumi Comeau <tk565319@dal.ca>
2023-02-23 23:47:05 -0400

    made code structure of previous implementation better

--- interact.py
+++ interact.py
@@ -22,3 +22,17 @@ def plant():
     pydirectinput.keyUp("z")
     time.sleep(random_breaks.paying_attention_break())
     water()
+
+
+def first_plant():
+    # get to the section where you have to pick your seeds
+    pydirectinput.keyDown("z")
+    time.sleep(random_breaks.plant_break())
+    pydirectinput.keyUp("z")
+    input("Hit anything once you have manually added the seeds you want to plant and hit 'plant seeds'")
+    # wait for user to switch back to game window
+    time.sleep(2)
+    pydirectinput.keyDown("z")
+    time.sleep(random_breaks.plant_break())
+    pydirectinput.keyUp("z")
+    water()
--- move.py
+++ move.py
@@ -61,18 +61,8 @@ def do_section(side, face, action):
         interact.water()
     # you have to manually plant your first seeds but the program will water for you
     elif action == "plant" and first_plant is True:
-        # get to the section where you have to pick your seeds
-        pydirectinput.keyDown("z")
-        time.sleep(random_breaks.plant_break())
-        pydirectinput.keyUp("z")
-        input("Hit anything once you have manually added the seeds you want to plant and hit 'plant seeds'")
-        # wait for user to switch back to game window
-        time.sleep(2)
-        pydirectinput.keyDown("z")
-        time.sleep(random_breaks.plant_break())
-        pydirectinput.keyUp("z")
+        interact.first_plant()
         first_plant = False
-        interact.water()
     elif action == "plant" and first_plant is False:
         interact.plant()
     for k in range(5):
