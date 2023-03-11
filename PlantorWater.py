4946fc96a2bc90faf4d4a4d444c4b958dd483436
Takumi Comeau <79553392+T3tsuo@users.noreply.github.com>
2023-03-11 19:50:48 -0400

    added run function in PlantorWater

--- PlantorWater.py
+++ PlantorWater.py
@@ -1,20 +1,15 @@
-import sys
 import time
 
 import mistralton, abundant_shrine
 
-try:
-    # try to grab input from command-line
-    place = int(sys.argv[1])
-    action = sys.argv[2]
-except IndexError:
+
+def run():
     # else then ask for input
     place = int(input("1 for Mistralton or 2 for Abundant Shrine: "))
     action = input("'water' or 'plant'?: ")
-
-# time for user to switch windows
-time.sleep(2)
-if place == 1:
-    mistralton.do_all(action)
-elif place == 2:
-    abundant_shrine.do_all(action)
+    # time for user to switch windows
+    time.sleep(2)
+    if place == 1:
+        mistralton.do_all(action)
+    elif place == 2:
+        abundant_shrine.do_all(action)
